5f0fd85a409c85c1aeb869ec71a4ae9dac0323ca
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-04-10 14:14:17 +0200

    feat(bin, index): init app entry point

---
+++ src/bin/page-loader.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import loader from '..';
+
+const run = () => {
+  loader(process.argv);
+};
+
+run();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const loader = (args) => {
+  // eslint-disable-next-line no-console
+  console.log(args);
+};
+
+export default loader;
