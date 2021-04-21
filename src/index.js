fafcc8e42cb149a24618e5cd5cab797d179c93ff
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-04-21 20:20:21 +0200

    feat(index): download page and save file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-const loader = (url, folder) => {
-  // eslint-disable-next-line no-console
-  console.log({ url, folder });
+import fs from 'fs';
+import path from 'path';
+import axios from 'axios';
+
+const loader = async (url, folder) => {
+  const filePath = path.resolve(__dirname, folder, 'github-com-rustamyusupov.html');
+
+  const response = await axios.get(url, { responseType: 'arraybuffer' });
+
+  await fs.promises.writeFile(filePath, response.data);
+
+  return filePath;
 };
 
 export default loader;
