f45ff695c5dd1ee1c13929874982cfa7cf0c5edf
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-05-02 12:02:50 +0200

    test(tests): update for getname

--- __tests__/getName.test.js
+++ __tests__/getName.test.js
@@ -1,7 +1,7 @@
 import getName from '../src/getName';
 
 describe('getName', () => {
-  it(`should return name`, () => {
+  it(`should return name for url`, () => {
     const url = 'https://ru.hexlet.io/courses';
     const result = getName(url);
     const expected = 'ru-hexlet-io-courses';
@@ -9,10 +9,10 @@ describe('getName', () => {
     expect(result).toBe(expected);
   });
 
-  it('should return name without hash', () => {
-    const url = 'https://ru.hexlet.io/courses#123';
+  it('should return name for path', () => {
+    const url = 'https://ru.hexlet.io/assets/professions/nodejs';
     const result = getName(url);
-    const expected = 'ru-hexlet-io-courses';
+    const expected = 'ru-hexlet-io-assets-professions-nodejs';
 
     expect(result).toBe(expected);
   });
