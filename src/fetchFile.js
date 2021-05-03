c80c53d6914870648710f4ec547c36409f564860
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-05-03 19:58:59 +0200

    feat(src): add response type for fetchfile

--- src/fetchFile.js
+++ src/fetchFile.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const fetchFile = async (url) => {
+const fetchFile = async (url, responseType) => {
   try {
-    const response = await axios.get(url, { responseType: 'text' });
+    const response = await axios.get(url, { responseType });
     const result = response.data;
 
     return result;
