34578ce5b94e1d9fc8fb90cc2f4caffee20c85a6
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-05-03 19:59:31 +0200

    feat(src): create files directory

--- src/index.js
+++ src/index.js
@@ -4,18 +4,34 @@ import path from 'path';
 import fetchFile from './fetchFile';
 import getName from './getName';
 
+// https://ru.hexlet.io/courses -> ru-hexlet-io-courses.html
+// https://ru.hexlet.io/courses -> ru-hexlet-io-courses_files
+// /assets/professions/nodejs.png -> ru-hexlet-io-assets-professions-nodejs.png
+
 const loader = async (url, folder) => {
   if (!url) {
     return '';
   }
 
-  const fileName = `${getName(url)}.html`;
-  const filePath = path.resolve(__dirname, folder, fileName);
+  const { host, pathname } = new URL(url);
+  const name = getName(`${host}${pathname}`);
+  const htmlName = `${name}.html`;
+  const filePath = path.resolve(__dirname, folder, htmlName);
+  const filesName = `${name}_files`;
+  const dirPath = path.resolve(__dirname, folder, filesName);
 
   try {
-    const fileData = await fetchFile(url);
-
+    // download html
+    const fileData = await fetchFile(url, 'text');
     fs.writeFileSync(filePath, fileData);
+
+    // create directory
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath);
+    }
+
+    // download resource files (images)
+    // replace urls for resources
   } catch (error) {
     throw new Error(error);
   }
