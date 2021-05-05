1087d40fe8b5b5943a5b6200bcd4ce1ef61d0e22
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-05-05 18:50:45 +0200

    fix(src): wait save resources before app finish

---
+++ src/downloadResource.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+
+import request from './request';
+import getName from './getName';
+
+const downloadResource = async (link, url, filesPath, log) => {
+  log(`fetch resource ${link}`);
+
+  const { host } = new URL(url);
+  const data = await request(`${url}/${link}`, 'arraybuffer');
+  const ext = path.extname(link);
+  const withoutExt = link.replace(ext, '');
+  const fileName = getName(`${host}/${withoutExt}`);
+  const filePath = `${filesPath}/${fileName}${ext}`;
+
+  log(`save resource ${fileName}`);
+  await fs.promises.writeFile(filePath, data);
+};
+
+export default downloadResource;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+import debug from 'debug';
 import fs from 'fs';
 import path from 'path';
 
-import downloadResources from './downloadResources';
+import downloadResource from './downloadResource';
 import getName from './getName';
 import replaceLinks from './replaceLinks';
 import request from './request';
 
-const loader = async (url, folder) => {
+const logger = debug('page-loader');
+
+const loader = async (url, folder, log = logger) => {
   if (!url) {
+    log('url is empty');
+
     return '';
   }
 
@@ -19,17 +24,22 @@ const loader = async (url, folder) => {
   const filesPath = path.resolve(__dirname, folder, filesName);
 
   try {
+    log(`fetch page ${url}`);
     const htmlData = await request(url, 'text');
 
     if (!fs.existsSync(filesPath)) {
+      log(`create directory ${filesPath}`);
       fs.mkdirSync(filesPath);
     }
 
+    log('replace links');
     const { data, links } = replaceLinks(htmlData, url);
 
+    log(`save page ${htmlPath}`);
     fs.writeFileSync(htmlPath, data);
 
-    await downloadResources(links, url, filesPath);
+    const promises = links.map((link) => downloadResource(link, url, filesPath, log));
+    await Promise.all(promises);
   } catch (error) {
     throw new Error(error);
   }
