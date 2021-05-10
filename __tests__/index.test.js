d0f436aa3255731030ce65368aae155dba092226
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-05-10 22:20:07 +0200

    test(tests): add name for resource test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,17 +11,17 @@ axios.defaults.adapter = require('axios/lib/adapters/http');
 const getFixture = (fileName) => path.join(__dirname, '../__fixtures__', fileName);
 const readFile = (filePath) => fs.readFile(filePath, 'utf-8');
 
-let tempDir = '';
 const origin = 'https://ru.hexlet.io';
 const pathname = '/courses';
 const url = `${origin}${pathname}`;
 const htmlFileName = 'ru-hexlet-io-courses.html';
-const filesName = 'ru-hexlet-io-courses_files';
+const filesFolder = 'ru-hexlet-io-courses_files';
 const serverResponse = {
   ok: 200,
   notFound: 404,
   serverError: 500,
 };
+let tempDir = '';
 
 const resources = [
   {
@@ -51,9 +51,9 @@ describe('index loader', () => {
       resource.data = data;
     };
 
-    const promises = resources.map(getData);
-    await Promise.all(promises);
+    await Promise.all(resources.map(getData));
   });
+
   afterAll(() => {
     nock.cleanAll();
     nock.enableNetConnect();
@@ -76,55 +76,15 @@ describe('index loader', () => {
     expect(result).toBe(expected);
   });
 
-  test.each(resources)('should return %p', async ({ name, data: expected }) => {
-    const filePath = `${tempDir}/${filesName}/${name}`;
-    const result = await readFile(filePath);
+  test.each(resources.map((r) => [r.name, r]))(
+    'should return %s',
+    async (_, { name, data: expected }) => {
+      const filePath = `${tempDir}/${filesFolder}/${name}`;
+      const result = await readFile(filePath);
 
-    expect(result).toBe(expected);
-  });
-
-  // it('should return files', async () => {
-  //   const htmlFile = await fs.readFile(getFixture('index.html'), 'utf-8');
-  //   const cssFile = await fs.readFile(getFixture('application.css'), 'utf-8');
-  //   const imgFile = await fs.readFile(getFixture('nodejs.png'), 'utf-8');
-  //   const jsFile = await fs.readFile(getFixture('runtime.js'), 'utf-8');
-
-  //   nock(origin)
-  //     .get(pathname)
-  //     .reply(serverResponse.ok, htmlFile)
-  //     .get('/assets/application.css')
-  //     .reply(serverResponse.ok, cssFile)
-  //     .get('/assets/professions/nodejs.png')
-  //     .reply(serverResponse.ok, imgFile)
-  //     .get('/packs/js/runtime.js')
-  //     .reply(serverResponse.ok, jsFile);
-
-  //   await loader(url, tempDir);
-
-  //   const htmlPath = path.join(tempDir, getName(url));
-  //   const htmlResult = await fs.readFile(htmlPath, 'utf-8');
-  //   const htmlExpected = await fs.readFile(getFixture('result.html'), 'utf-8');
-
-  //   expect(htmlResult).toBe(htmlExpected);
-
-  //   const assetsPaths = [
-  //     '/assets/application.css',
-  //     '/assets/professions/nodejs.png',
-  //     '/packs/js/runtime.js',
-  //   ];
-
-  //   const test = async (file) => {
-  //     const fileName = getName(`${origin}${file}`);
-  //     const filePath = path.join(tempDir, 'ru-hexlet-io-courses_files', fileName);
-  //     const result = await fs.readFile(filePath, 'utf-8');
-  //     const expected = await fs.readFile(getFixture(file), 'utf-8');
-
-  //     expect(result).toBe(expected);
-  //   };
-
-  //   const promises = assetsPaths.map(test);
-  //   await Promise.all(promises);
-  // });
+      expect(result).toBe(expected);
+    }
+  );
 
   it('should reject with 404', async () => {
     const scope = nock(origin).get('/notFound').reply(serverResponse.notFound);
